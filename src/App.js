3bbb895a3fcaabf762b4d78f82be5d4f9924b124
majdiShomali <majdi.shomali.1997@gmail.com>
2023-05-16 11:31:52 +0300

    edit search

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
 import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-
+let DataW2=[]
 function App() {
   const[Data,setData] = useState(null)
+  const [searchTerm, setSearchTerm] = useState('');
+  const [FilterData, setFilterData] = useState();
+
   const fetchData =()=>{
   axios.get('http://localhost:5000/')
   .then(res => {
   setData(res.data) 
+  DataW2=res.data
+  setFilterData(res.data)
   })
   }
   useEffect(()=>{
     fetchData()
+   
   },[])
 
   const filterDataByName = (searchTerm) => {
-    const filteredResults = Data.filter(item => item.title.includes(searchTerm));
-    setData(filteredResults);
+     const filteredData = Data.filter(item =>
+       item.title.toLowerCase().includes(searchTerm.toLowerCase())
+     );
+         setFilterData(filteredData);
   }
- 
+
+  
     return (
       <>
 <input name="firstName"
- onChange={(e) => filterDataByName(e.target.value)}
+value={searchTerm}
+ onChange={(e) =>{
+  setSearchTerm(e.target.value);
+   filterDataByName(e.target.value);
+  }
+}
  />     
   <div className='container'>
    {
-   Data?.map((e)=>{
+   FilterData?.map((e)=>{
     return(
     <div className='card'>
+
       <p>{e.title}</p>
       <img src={e.thumbnail}></img>
       <p>{e.description}</p>
 
     </div>
-
-    )
-      
+    )   
         })
    }
-
    </div>
       </>
      )
 
-
-
-
- 
-
- 
 }
 export default App;
